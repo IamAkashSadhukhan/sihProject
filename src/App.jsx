af747e7bdbc0c888b59166c9995133cf0e3800f3
Itadi02 <adityashaw053@gmail.com>
2025-09-03 22:26:25 +0530

    Diseasedetectionpage completed!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Page2 from "./components/Page2";
+import DiseaseDetection from "./components/DiseaseDetection";
 const App = () => {
   return (
     <div>
-      <Page2 />
+      {/* <Page2 /> */}
+      <DiseaseDetection />
     </div>
   );
 };
