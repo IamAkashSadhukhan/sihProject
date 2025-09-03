0d575831cb53d8c8254aa950f458095ab6af2110
Itadi02 <adityashaw053@gmail.com>
2025-09-03 23:22:39 +0530

    PaddyPage completed!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Page2 from "./components/Page2";
 import DiseaseDetection from "./components/DiseaseDetection";
+import PaddyPage from "./components/PaddyPage";
 const App = () => {
   return (
     <div>
       {/* <Page2 /> */}
-      <DiseaseDetection />
+      {/* <DiseaseDetection /> */}
+      <PaddyPage />
     </div>
   );
 };
